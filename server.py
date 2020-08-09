b04764ab9ec3949252d98b2577de1a26eac573b9
Ruan Pearce-Authers <ruanpa@outlook.com>
2020-08-09 13:54:48 +0100

    Initial commit

---
+++ server.py
@@ -0,0 +1,34 @@
+from dataclasses import dataclass
+from typing import List
+
+from govyn import create_app, run_app
+
+@dataclass
+class AddRequest:
+	numbers: List[int]
+
+@dataclass
+class Response:
+	result: int
+
+class CalculatorAPI:
+	# run initialisation tasks, e.g. connecting to a database
+	async def startup(self) -> None:
+		pass
+
+	# be a good citizen and dispose of things appropriately
+	async def shutdown(self) -> None:
+		pass
+
+	# get_ methods take a query string
+	# callers will receive a 400 Bad Request if they supply invalid values
+	async def get_add(self, a: int, b: int) -> Response:
+		return Response(a + b)
+
+	# post_ methods take a JSON request body
+	# also type-checked according to the dataclass definition
+	async def post_add(self, req: AddRequest) -> Response:
+		return Response(sum(req.numbers))
+
+app = create_app(CalculatorAPI())
+run_app(app)
